6f719c14144ce081d69dd86cf149b940fa8982e4
threezinedine <threezinedine@gmail.com>
2024-08-05 13:31:04 +0700

    simple setup for the game with just a window and a simple circle drawn on it

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 int main(int argc, char **argv)
 {
-#ifdef _DEBUG
-    std::cout << "Debug mode\n";
-#else
-    std::cout << "Release mode\n";
-#endif
-    std::cout << "Hello world\n";
-    return 0;
+    sf::RenderWindow window(sf::VideoMode({640, 480}), "SFML works");
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition({100, 200});
+
+    while (window.isOpen())
+    {
+        // Process events
+        while (const auto &event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+
+            if (event->is<sf::Event::KeyPressed>())
+            {
+                auto keyEvent = event->getIf<sf::Event::KeyPressed>();
+
+                if (keyEvent->code == sf::Keyboard::Key::Escape)
+                {
+                    window.close();
+                }
+            }
+        }
+
+        // Update
+
+        // Draw
+        window.clear(sf::Color::Black);
+        window.draw(shape);
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
 }
